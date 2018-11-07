0721cc087adbe79bbbeb5395994d6f4f3b9f8679
ir <kakuzamu0718874@gmail.com>
2018-11-07 11:57:36 +0900

    implementation returnlabel

--- src/db/httpserver.js
+++ src/db/httpserver.js
@@ -1,23 +1,26 @@
 const promana = require('./progress-manage')
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
-const port = 8888
 var pre_label = ""
 
-app.listen(port)
-console.log('Server is started')
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.listen(8888)
 
 app.post('/returnlabel',function(req, res){
     var next_label
     var player = req.get('Player')
-    var buttom = "bt1"
-    var label  = "lab1"
-    next_label = promana.returnLabel(1,1,1)
+    var buttom = req.body.buttom
+    var label  = req.body.label
+    next_label = promana.returnLabel(player,buttom,label)
     if(pre_label === next_label){
         // do nothing
+        res.end()
     }else{
         // send label
+        res.status(200).send(next_label)
         pre_label = next_label
     }
 })
